a09e297e8e3489016ddc66844cd578beea2c4efa
joaodagaita <joaovsilva70@outlook.com>
2024-10-16 17:47:22 -0300

    adding chart js and setup home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,18 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 export default function Home() {
+  const loggedin = {firstName: 'João'}
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox subtext="Access and manage your account and transactions efficiently" type="greeting" title="Welcome" user={loggedin?.firstName || 'Guest'} />
+
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250} />
+        </header>
+      </div>
+    </div>
   )
 }
